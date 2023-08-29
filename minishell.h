b2eb3a2ad7cf4fedcd80b56d462fb769f33ab286
Polina Simonenko <rabarbrablad@gmail.com>
2023-08-29 15:47:27 +0200

    added header file

---
+++ minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: psimonen <psimonen@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/29 15:45:59 by psimonen          #+#    #+#             */
+/*   Updated: 2023/08/29 15:46:45 by psimonen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+# include <unistd.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft.h"
+#endif
