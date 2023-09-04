06fc7bc667b4bd71d2f7be9fd411381f55ada09c
fbohling <fbohling@student.42wolfsburg.de>
2023-09-04 21:40:18 +0200

    Update: export.c

--- builtins/export.c
+++ builtins/export.c
@@ -1,6 +1,11 @@
 #include "../minishell.h"
 #include "../errnu.h"
 
+bool	export(t_builtins *data, char *temp)
+{
+	if (!temp || !*temp)
+}
+
 int	main(int argc, char **argv, char **envp)
 {
 	print_env(envp);
