96d177c310ddda80597ba4b545cec75dd29db733
Polina Simonenko <rabarbrablad@gmail.com>
2023-09-11 19:12:59 +0200

    'executor - in process'

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: psimonen <psimonen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 13:17:04 by psimonen          #+#    #+#             */
-/*   Updated: 2023/09/01 12:36:11 by psimonen         ###   ########.fr       */
+/*   Updated: 2023/09/11 18:59:37 by psimonen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ void	handler(int signum)
 		//tputs(tgetstr("ce", NULL), 1, ft_new_putchar);
 		printf("\n");
 		rl_on_new_line();
-		rl_replace_line("", 0);
+		//rl_replace_line("", 0);
 		rl_redisplay();
 		return ;
 	}
