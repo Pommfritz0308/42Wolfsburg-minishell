7d52d76140125d91df530fd627c58eb39a7e7fd6
Polina Simonenko <rabarbrablad@gmail.com>
2023-09-17 13:09:00 +0200

    some fixes

--- errnu.h
+++ errnu.h
@@ -1,5 +1,9 @@
 #ifndef ERRNU_H
 # define ERRNU_H
+# define PIPE_ERR	107
+# define PIPE_ERR_T	"wrong pipe usage"
 # define SYNTAX		108
 # define SYNTAX_T	"syntax error near unexpected token `&'"
+# define COND_ERR	109
+# define COND_ERR_T	"wrong condition usage"
 #endif
