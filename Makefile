034e73f9864e91ee691322b1cab7eb78ca9633a6
Frederik Bohling <fbohling@student.42wolfsburg.de>
2023-09-26 14:32:40 +0200

    Attempt 1 wildcards

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: fbohling <fbohling@student.42wolfsburg.    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/04/18 18:32:16 by fbohling          #+#    #+#              #
-#    Updated: 2023/09/25 18:57:12 by fbohling         ###   ########.fr        #
+#    Updated: 2023/09/26 14:33:23 by fbohling         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -21,7 +21,7 @@ SRC = 			main resolve_env resolve_env_utils init exec_builtin signals \
 				tokenizer tokenizer_utils executor executor_utils env_utils \
 				str_utils str_utils_2 str_utils_3 tree_utils tree_utils_2 \
 				redirections redirections_utils parser parser_utils heredoc \
-				path clean clean_2 fd_utils errors debug
+				path clean clean_2 fd_utils errors debug wildcards
 BUILTINS =		export export_2 pwd env cd cd_2 unset echo exit exit2
 
 OBJS =			$(addprefix $(OBJ_DIR)/, $(addsuffix .o, $(SRC)))
--- srcs/wildcards.c
+++ srcs/wildcards.c
@@ -12,7 +12,7 @@ char	*dir_iteri(struct dirent *dir, DIR *d, char *wildcard)
 		dir = readdir(d);
 		while (dir)
 		{
-			if (check_wildcard(dir->d_name, wildcard))
+			if (check(wildcard, dir->d_name))
 			{
 				tmp = ft_strjoin(res, dir->d_name);
 				res = ft_strjoin(tmp, " ");
@@ -62,4 +62,74 @@ char	*resolve_wildcards(char *wildcard)
 	res = dir_iteri(&dir, d, wildcard);
 	closedir(d);
 	return (res);
+}
+
+int	check(char *wild, char *dir)
+{
+	size_t	i;
+	char	*temp;
+	int		pos;
+	int		len;
+
+	i = 0;
+	while (wild[i] == '*')
+		i++;
+	if (i == ft_strlen(wild))
+		return (true);
+	i = 0;
+	temp = NULL;
+	pos = 0;
+	len = 0;
+	if (wild[0] != '*')
+	{
+		while (dir[i] == wild[i])
+			i++;
+		if (wild[i] != '*')
+			return (false);
+		else
+			return (check(wild + i, dir + i));
+	}
+	else
+	{
+		while (wild[i] == '*')
+			i++;
+		if (!ft_strchr(wild + i, '*'))
+		{
+			if (rev_search_str(dir, wild))
+				return (true);
+			else
+				return (false);
+		}
+		else
+		{
+			temp = ft_substr(wild + i, 0, ft_find_pos(wild + 1, '*') - 1);
+			len = ft_strlen(temp);
+			pos = ft_find_substr(dir, temp);
+			if (pos == -1)
+				return (free(temp), false);
+			else
+				return (free(temp), check(wild + ft_find_pos(wild + 1, '*'), dir + (pos + len))); // continue at wild[i] == '*' & dir[i] == end of match
+		}
+
+	}
+}
+
+int	rev_search_str(char *dir, char *wild)
+{
+	int	i;
+	int	j;
+
+	i = ft_strlen(dir);
+	j = ft_strlen(wild);
+
+	while (wild[j] != '*' && i >= 0)
+	{
+		if (wild[j] != dir[i])
+			return (false);
+		j--;
+		i--;
+	}
+	if (wild[j] == '*')
+		return (true);
+	return (false);
 }
